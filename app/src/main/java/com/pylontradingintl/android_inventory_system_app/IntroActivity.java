b355d14bccd12dfd43d3217df82066822aa4d881
Joseph <joseph.larracas098@gmail.com>
2021-07-24 11:08:54 +0800

    Update

--- app/src/main/java/com/pylontradingintl/android_inventory_system_app/IntroActivity.java
+++ app/src/main/java/com/pylontradingintl/android_inventory_system_app/IntroActivity.java
@@ -1,23 +1,59 @@
 package com.pylontradingintl.android_inventory_system_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static androidx.core.view.WindowInsetsCompat.Type.systemBars;
+import static androidx.core.view.WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE;
 
 public class IntroActivity extends AppCompatActivity {
 
     private ViewPager screenPager;
     IntroViewPagerAdapter introViewPagerAdapter;
+    TabLayout tabIndicator;
+    Button btnNext;
+    int position = 0;
+    Button btnGetStarted;
+    Animation btnAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        hideSystemBars();
+
+        if(restorePrefData()){
+            Intent mainActivity = new Intent(getApplicationContext(),AdminPanel.class);
+            startActivity(mainActivity);
+            finish();
+        }
+
+
+
+        tabIndicator = findViewById(R.id.tab_indicator);
+        btnAnim = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.button_animation);
 
 
         List<ScreenItem> mList = new ArrayList<>();
@@ -27,5 +63,84 @@ public class IntroActivity extends AppCompatActivity {
         screenPager = findViewById(R.id.screen_viewpager);
         introViewPagerAdapter = new IntroViewPagerAdapter(this,mList);
         screenPager.setAdapter(introViewPagerAdapter);
+
+        btnNext = findViewById(R.id.btn_next);
+        btnGetStarted = findViewById(R.id.btn_getStarted);
+        tabIndicator.setupWithViewPager(screenPager);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position = screenPager.getCurrentItem();
+                if(position < mList.size()){
+                    position++;
+                    screenPager.setCurrentItem(position);
+                }
+                if(position == mList.size() - 1){
+                    loadLastScreen();
+                }
+            }
+        });
+
+        tabIndicator.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                if(tab.getPosition() == mList.size() - 1){
+                    loadLastScreen();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        btnGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mainActivity = new Intent(getApplicationContext(),AdminPanel.class);
+                startActivity(mainActivity);
+                savePrefData();
+                finish();
+            }
+        });
+    }
+
+    private boolean restorePrefData() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        Boolean isIntroActivityOpenedBefore = pref.getBoolean("isIntroOpened",false);
+        return isIntroActivityOpenedBefore;
+    }
+
+    private void savePrefData() {
+        SharedPreferences preferences = getApplicationContext().getSharedPreferences("myPrefs",MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("isIntroOpened",true);
+        editor.commit();
+    }
+
+
+    private void loadLastScreen() {
+        btnNext.setVisibility(View.INVISIBLE);
+        btnGetStarted.setVisibility(View.VISIBLE);
+        tabIndicator.setVisibility(View.INVISIBLE);
+        btnGetStarted.setAnimation(btnAnim);
+    }
+
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat insetsControllerCompat = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        insetsControllerCompat.setSystemBarsBehavior(BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        insetsControllerCompat.hide(systemBars());
+    }
+
+    private void showSystemBars() {
+        WindowInsetsControllerCompat insetsControllerCompat = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        insetsControllerCompat.show(systemBars());
     }
 }
